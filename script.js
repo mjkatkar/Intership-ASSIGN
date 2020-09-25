7551df039beed8de348f55b1fe85b9f701cbdc3b
mjkatkar <67830645+mjkatkar@users.noreply.github.com>
2020-09-25 15:23:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var header = document.getElementById("tabs");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
